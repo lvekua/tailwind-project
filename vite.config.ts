3437ec3506363a8bb809fdbc25dbb27366aa5288
Levan Vekua <lvekua@gmail.com>
2022-04-15 08:42:40 -0400

    Compressed HTML, CSS

---
+++ vite.config.ts
@@ -0,0 +1,10 @@
+import { defineConfig, Plugin } from 'vite'
+import { createHtmlPlugin } from 'vite-plugin-html'
+
+export default defineConfig({
+  plugins: [
+    createHtmlPlugin({
+      minify: true,
+    })
+  ]
+});
